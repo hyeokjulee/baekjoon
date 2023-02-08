3c526fe214d5467ccc2a6d3f1822716708966904
hyeokjulee <dlhjwwe@gmail.com>
2023-02-08 19:24:52 +0900

    ver 230208

--- vscode/24025.py
+++ vscode/24025.py
@@ -2,36 +2,57 @@ import sys
 
 n = int(sys.stdin.readline()) # 무리의 수 n 입력
 a = list(map(int, sys.stdin.readline().split())) # 시야점수의 조건을 나타내는 배열 a 입력
-b = a.copy()
 o = [0 for _ in range(n)] # 정령 무리들의 키가 들어갈 리스트
+
+y = [[0 for col in range(2)] for row in range(n)]
 for i in range(n):
-  if max(b) > 0:
-    o[b.index(max(b))] = n - i
-    b[b.index(max(b))] = 0
-  else:
-    o[b.index(min(b))] = n - i
-    b[b.index(min(b))] = 0
+    y[i][0] = a[i]
+    y[i][1] = i
+y.sort()
+y.reverse()
+
+
+for i in range(n):
+    if y[i][0] > 0:
+        o[y[i][1]] = n - i
+        y[i][0] = 0
+
+y.reverse()
+    
+for i in range(n):
+    if y[i][0] < 0:
+        o[y[i][1]] = i + 1
+        y[i][0] = 0
 
 
 # 점수 처리
 k = [0 for _ in range(n)] # 정령 무리들의 점수가 들어갈 리스트
-max = o[n - 1] # 마지막 정령 무리의 키가 현재 최고 키
-for i in range(n-1,-1,-1):
-    if o[i] < max:
-        k[i] = o.index(max) - i
-    else: # 모두 볼 수 있다면
-        max = o[i]
-        k[i] = 1000000000
-    k.reverse() # 배열 요소들의 순서를 뒤집기
+maxIndexOfO = n - 1 # 마지막 정령 무리의 키가 현재 최고 키
 
+m = [[0 for col in range(2)] for row in range(n)]
+for i in range(n):
+    m[i][0] = o[i]
+    m[i][1] = i
+
+for i in range(n-1,-1,-1):
+    if o[i] < m[maxIndexOfO][0]:
+        k[i] = m[maxIndexOfO][1] - i
+    else: # 모두 볼 수 있다면
+        maxIndexOfO = i
+        k[i] = 1000000000
+
+k.reverse() # 배열 요소들의 순서를 뒤집기
+
+answer = ""
 
 for i in range(n): # 조건 확인 후 출력
-    if (a[i] < 0 or k[i] < a[i]) and (a[i] > 0 or k[i] > -a[i]) or a[n - 1] < 0: # 조건에 만족하지 않으면
+    if (a[i] < 0 and k[i] <= -a[i]) or (a[i] > 0 and k[i] >= a[i]): # 조건에 만족하면
+        answer += str(o[i])
+        if i < n - 1:
+            answer += ' '
+        else: # 정령무리들 모두 만족한 거면
+            print(answer)
+            sys.exit()
+    else: # 조건에 만족하지 않으면
         print(-1)
         sys.exit()
-    else: # 조건에 만족하면
-        if i == n - 1: # 근데 정령무리들 모두 만족한 거면
-            answer = ""
-            for i in range(n):
-                answer += str(o[i]) + ' '
-            print(answer)
