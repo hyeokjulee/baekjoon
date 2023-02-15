3e0927ffcdc2de06728f66a6823d82a64df42c67
hyeokjulee <dlhjwwe@gmail.com>
2023-02-16 00:43:39 +0900

    ex 230215

---
+++ python/ex16505.py
@@ -0,0 +1,29 @@
+import sys, copy
+
+def listMtoN(x): # 재귀함수
+    if x == 0:
+        return ['*']
+
+    listM = listMtoN(x - 1)
+
+    listN = []
+    for i in range(2 ** x, 0, -1):
+        for j in i:
+            listN.append(' ')
+
+    for i in range(2 ** (x - 1), 0, -1):
+        for j in i:
+            listN[i][j] = listM[i][j]
+            listN[i + 2 ** (x - 1)][j] = listM[i][j]
+            listN[i][j + 2 ** (x - 1)] = listM[i][j]
+
+    return listN
+
+N = int(sys.stdin.readline())
+
+outList = listMtoN(N)
+
+for i in range(2 ** N, 0, -1):
+    for j in i:
+        print(outList[i][j], end='')
+    print()
