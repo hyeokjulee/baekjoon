ccd05d8acae12381ae446a34daf1dbdbd52b9739
hyeokjulee <dlhjwwe@naver.com>
2023-02-18 18:33:11 +0900

    ver 230218

--- python/ex16505.py
+++ python/ex16505.py
@@ -1,30 +1,30 @@
 import sys
 
-def listMtoN(x): # 재귀함수
+def listMtoN(x): # 재귀 함수
     if x == 0:
-        return [['*']]
+        return [['*']] # listMtoN(0) = [['*']]
 
-    listM = listMtoN(x - 1).copy()
+    listM = listMtoN(x - 1) # 재귀 호출
 
-    listN = [[] for i in range(2 ** x)]
+    listN = [[] for i in range(2 ** x)] # 리스트 생성
 
-    for i in range(2 ** x - 1, -1, -1):
-        for j in range(i + 1):
-            listN[2 ** x - 1 - i].append(' ')
+    for i in range(2 ** x , 0, -1):
+        for j in range(i):
+            listN[2 ** x - i].append(' ') # 리스트의 값들은 일단 모두 공백으로 초기화하며 전체적인 모양의 틀을 잡는다.
 
-    for i in range(2 ** (x - 1) - 1, -1, -1):
-        for j in range(i + 1):
-            listN[2 ** (x - 1) - 1 - i][j] = listM[2 ** (x - 1) - 1 - i][j]
-            listN[2 ** x - 1 - i][j] = listM[2 ** (x - 1) - 1 - i][j]
-            listN[2 ** (x - 1) - 1 - i][j + 2 ** (x - 1)] = listM[2 ** (x - 1) - 1 - i][j]
+    for i in range(2 ** (x - 1), 0, -1):
+        for j in range(i):
+            listN[2 ** (x - 1) - i][j] = listM[2 ** (x - 1) - i][j] # 좌측 상단 부분 채워줌
+            listN[2 ** (x - 1) - i][j + 2 ** (x - 1)] = listM[2 ** (x - 1) - i][j] # 우측 상단 부분 채워줌
+            listN[2 ** x - i][j] = listM[2 ** (x - 1) - i][j] # 좌측 하단 부분 채워줌
 
     return listN
 
-N = int(sys.stdin.readline())
+N = int(sys.stdin.readline()) # 정수 입력
 
-outList = listMtoN(N)
+outList = listMtoN(N) # 재귀 함수 실행
 
-for i in range(2 ** N - 1, -1, -1):
-    for j in range(i + 1):
+for i in range(2 ** N, 0, -1):
+    for j in range(i):
         print(outList[2 ** N - i][j], end='')
     print()
