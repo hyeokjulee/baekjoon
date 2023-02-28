bf670286bb8117889f9d43c9dd2f2f5af608e1aa
hyeokjulee <dlhjwwe@gmail.com>
2023-02-28 18:52:04 +0900

    ver 230228

--- python/ex3987.py
+++ python/ex3987.py
@@ -9,10 +9,36 @@ for i in range(N):
 
 PR, PC = map(int, sys.stdin.readline().split())
 
-space[PR - 1][PC - 1] = 'S'
+position = [PR - 1, PC - 1]
 
 print()
 for i in range(N):
     for j in range(N):
         print(space[i][j], end='')
     print()
+
+# 위
+cnt = 0
+direction = 'U'
+flag = True
+while flag:
+    if direction == 'U':
+        if position[0] != 0:
+            position = [position[0] - 1][position[1]]
+        if position[0] == 0 or position[0] == 'C':
+            flag = False
+        elif space[position[0]][position[1]] == '.':
+            cnt += 1
+        elif space[position[0]][position[1]] == '/':
+            cnt += 1
+            position = [position[0] - 1][position[1]]
+            direction = 'R'
+        elif space[position[0]][position[1]] == '\\':
+            cnt += 1
+            direction = 'L'
+    if direction == 'R':
+
+
+# 오른쪽
+# 아래
+# 왼쪽 (U: 위, R: 오른쪽, D: 아래, L: 왼쪽)
