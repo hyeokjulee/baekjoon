78938b44cbdbc51f0be1f46f947bfb1307cc611c
HyeokJu Lee <dlhjwwe@naver.com>
2024-07-01 17:15:40 +0900

    v240701 5

--- python/ex1182.py
+++ python/ex1182.py
@@ -1,20 +1,25 @@
 import sys
+from itertools import combinations
 
 N, S = map(int, sys.stdin.readline().split())
-nums = tuple(map(int, sys.stdin.readline().split()))
+numbers = tuple(map(int, sys.stdin.readline().split()))
 cnt = 0
+
+# for i in range(1, N + 1):
+#     for j in combinations(numbers, i):
+#         if sum(j) == S:
+#             cnt += 1
+# print(cnt)
 
 def func(idx, sum):
     if idx < N:
         func(idx + 1, sum)
-        func(idx + 1, sum + nums[idx])
+        func(idx + 1, sum + numbers[idx])
     else:
         if sum == S:
             global cnt
             cnt += 1
-
 func(0, 0)
-
 if S != 0:
     print(cnt)
 else:
