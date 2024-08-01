8dff70a3f7e00a02bd206d89b93bdde86a643322
HyeokJu Lee <dlhjwwe@naver.com>
2024-08-01 17:49:39 +0900

    DP 문제

--- python/ex2293.py
+++ python/ex2293.py
@@ -1,24 +1,14 @@
 import sys
 
 n, k = map(int, sys.stdin.readline().split())
-coins = [] # 동전값리스트
+a = []
 for _ in range(n):
-    val = int(sys.stdin.readline())
-    if val <= k: # k원 이하의 동전들만 취급
-        coins.append(val)
-coins.sort() # 동전값리스트 오름차순정렬
+    a.append(int(sys.stdin.readline()))
 
-dp = [set()] # dp테이블 : 0번 인덱스값은 빈셋을 넣어줌
-for _ in range(k):
-    dp.append(set()) # dp테이블은 일단 빈 셋으로 채워놓아줌 : dp[가치의합] = 조건을 충족하는 튜플들의 셋 (튜플들은 오름차순정렬해서 넣어야함)
+d = [0] * (k + 1)
+d[0] = 1 
+for i in a:
+    for j in range(i, k + 1):
+        d[j] += d[j - i]
 
-for i in range(1, k + 1):
-    for coin in coins:
-        if coin == i: # 인덱스와 동전값이 같은 경우에
-            dp[i].add((coin,)) # 동전값만 들어있는 튜플을 셋에넣음
-        elif coin < i: # 지금인덱스보다 작은 동전값들의 경우에
-            for tup in dp[i - coin]: # 각각 동전값들을 뺀 가격인덱스의 튜플들을 가져오고
-                dp[i].add(tuple(sorted(list(tup + (coin,))))) # 가져온 튜플들에 동전값을 추가한 후 오름차순정렬해서 셋에 넣음
-    print(i, ' : ', len(dp[i]))
-
-print(len(dp[k]))
+print(d[k])
