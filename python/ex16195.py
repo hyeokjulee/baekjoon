da25de5d98b7e2bed1b5e173bb04f5daf1033f3f
HyeokJu Lee <dlhjwwe@naver.com>
2024-08-04 18:56:45 +0900

    DP 문제

---
+++ python/ex16195.py
@@ -0,0 +1,17 @@
+import sys
+
+d = [[0], [1], [1, 1], [1, 2, 1], [0, 3, 3, 1]]
+for i in range(5, 1001):
+    arr = [0] * i
+    for j in range(1, i - 2):
+        arr[j] = (d[i - 1][j - 1] + d[i - 2][j - 1] + d[i - 3][j - 1]) % 1000000009
+    arr[i - 2], arr[i - 1] = i - 1, 1
+    d.append(arr)
+
+T = int(sys.stdin.readline())
+for _ in range(T):
+    n, m = map(int, sys.stdin.readline().split())
+    sum = 0
+    for i in range(m):
+        sum += d[n][i]
+    print(sum % 1000000009)
