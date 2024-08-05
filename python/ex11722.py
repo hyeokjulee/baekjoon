8c3f4ab46436e63cc29fbe91db65883a7965cb9e
HyeokJu Lee <dlhjwwe@naver.com>
2024-08-05 12:14:14 +0900

    DP 문제

---
+++ python/ex11722.py
@@ -0,0 +1,12 @@
+import sys
+
+N = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().split()))
+arr = [1] * N
+
+for i in range(1, len(A)):
+    for j in range(i):
+        if A[i] < A[j] and arr[j] + 1 > arr[i]:
+            arr[i] = arr[j] + 1
+
+print(max(arr))
