2bd3f43e853a408652449430fd9dc68fa4c4ef61
HyeokJu Lee <dlhjwwe@naver.com>
2024-08-06 11:51:34 +0900

    이분탐색 문제

---
+++ python/ex14003.py
@@ -0,0 +1,28 @@
+import sys, bisect
+
+N = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().split()))
+
+result = []
+arr = []
+d = []
+length_of_arr = 0
+for i in range(N):
+    idx = bisect.bisect_left(arr, A[i])
+    if idx == length_of_arr:
+        arr.append(A[i])
+        length_of_arr += 1
+        d.append([idx, A[i]])
+    elif arr[idx] != A[i]:
+        arr[idx] = A[i]
+        d.append([idx, A[i]])
+
+last_idx = max(d)[0]
+for i in range(len(d) - 1, -1, -1):
+    if d[i][0] == last_idx:
+        result.append(d[i])
+        last_idx -= 1
+
+print(length_of_arr)
+for i in range(len(result) - 1, -1, -1):
+    print(result[i][1], end=' ')
